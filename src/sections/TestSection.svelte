8ee9b9101399d931fe374203cfd6430ee5419801
TamaraLandman <tamaraahuvalandman@gmail.com>
2025-07-17 00:06:46 -0400

    new commit

---
+++ src/sections/TestSection.svelte
@@ -0,0 +1,37 @@
+<script>
+import Scroller2 from "../lib/Scroller2.svelte";
+import ArticleText from "../lib/ArticleText.svelte";
+</script>
+
+<div>
+ <!-- <Scroller layout="left">
+    {#snippet sticky()}
+       <img class="duck-img" src="duck.png" alt="KWK rubber duck!" />
+    {/snippet}
+
+    {#snippet scrolly()}
+        <ArticleText>
+        <h1>Testing this out!!!</h1>
+        </ArticleText>
+    {/snippet}
+ </Scroller> -->
+
+
+    <Scroller2>
+
+        {#snippet scrolly()}
+        <ArticleText>
+        <h1>Testing this out!!!</h1>
+        </ArticleText>
+        {/snippet}
+
+
+        {#snippet sticky()}
+       <img class="duck-img" src="duck.png" alt="KWK rubber duck!" />
+        {/snippet}
+
+
+
+    </Scroller2>
+
+</div>
